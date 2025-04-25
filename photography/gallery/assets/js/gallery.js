8e441eeba1d2a3ed25bc27d992a5324c038fb2d5
Alfie Kunz <alfiekunz1@gmail.com>
2025-04-25 16:16:43 +0100

    Main Page Update & Text Tweaking

--- photography/gallery/assets/js/gallery.js
+++ photography/gallery/assets/js/gallery.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const categoryHeader = {
     ball: {
         title: "Gallery -<br>Balls & Formals",
-        description: "Whether it be photos of groups, candids, awards, speeches or the venue, I strive to showcase the excitement and wholeness at an event to remember. I excel in busy situations and when meeting new people, and pride myself on building a friendly and charismatic rapport with guests while maintaining professionism and high directoral skills."
+        description: "Whether it be photos of groups, candids, awards, speeches or the venue, I strive to showcase the excitement and atmosphere of an event to remember. I excel in busy situations and when meeting new people, and pride myself on building a friendly and charismatic rapport with guests while maintaining professionalism and strong directorial skills."
     }
   };
   const headerContent = categoryHeader[category]
